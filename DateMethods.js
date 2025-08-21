acb8ac78f5c59aa7b2a3e703d29fb3b614152e83
SXP42750 <111460531+SXP42750@users.noreply.github.com>
2025-08-21 11:10:09 +0530

    Dates methods in javascript

---
+++ DateMethods.js
@@ -0,0 +1,14 @@
+const toDay = new Date()
+
+// console.log(toDay.getMonth()+1)
+
+// console.log(toDay.getFullYear())
+
+//set date
+
+
+toDay.setFullYear(2025);           // Change the year to 2024
+toDay.setMonth(10);                 // January (months are 0-indexed)
+toDay.setDate(15);                 // Set day of the month to 15
+console.log(toDay);                // 2024-01-15T...
+
